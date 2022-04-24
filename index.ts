0b41f954e4db9c93302210fe9c29b304359186fd
pramitha1166 <pramitha.wanigarathne@gmail.com>
2022-04-24 23:38:16 +0530

    init npm

--- index.ts
+++ index.ts
@@ -2,3 +2,19 @@ function hello(text: any) {
     console.log(text)
 }
 hello("react-hook-tool")
+
+const target= {
+    message1: "Hello",
+    message2: "Everyone"
+} 
+
+const handler = {
+    get(target, prop, reveiver) {
+        return "Worlds";
+    }
+}
+
+const proxy1 = new Proxy(target, handler);
+
+console.log(proxy1.message1)
+console.log(proxy1.message2)
