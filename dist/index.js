f8ed80a34425cff196dc3afec91e2b1c6a3056f0
pramitha1166 <pramitha.wanigarathne@gmail.com>
2022-04-26 10:34:40 +0530

    setup with typescript

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+function hello(text) {
+    console.log(text);
+}
+hello("react-hook-tool");
+const target = {
+    message1: "Hello",
+    message2: "Everyone"
+};
+const handler = {
+    get(target, prop, reveiver) {
+        return "Worlds";
+    }
+};
+const proxy1 = new Proxy(target, handler);
+console.log(proxy1.message1);
+console.log(proxy1.message2);
+//# sourceMappingURL=index.js.map
