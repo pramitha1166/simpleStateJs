b58e48d1c7f7198e699dd0cf9f990d1811c6d815
pramitha1166 <pramitha.wanigarathne@gmail.com>
2022-04-27 09:55:09 +0530

    implement pub sub pattern

--- pubsub.ts
+++ pubsub.ts
@@ -13,14 +13,20 @@ export default class PubSub {
         PubSub.events[eventName].push(callback); 
     }
 
-    public static unsubscribe() {
-
+    public static unsubscribe(eventName : string, callback : CallBackFunc) {
+        if(PubSub.events[eventName]) {
+            for (let i=0; i < PubSub.events[eventName].length; i++) {
+               if(PubSub.events[eventName][i]===callback) {
+                   PubSub.events[eventName].splice(i, 1);
+               }
+            }
+        }
     }
 
     public static publish(eventName : string, data : any) {
         if(PubSub.events[eventName]) {
-            PubSub.events[eventName].forEach(fun => {
-                fun(data);
+            PubSub.events[eventName].forEach(function(fn: CallBackFunc) {
+                fn(data);
             });
         }
     }
